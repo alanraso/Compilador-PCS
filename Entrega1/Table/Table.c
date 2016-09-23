948b365c1ea14b051fc05461c204cf8c99ca5b1d
alanraso <alanrraso@gmail.com>
2016-09-23 18:14:05 -0300

    Tratamento de comentarios

--- Entrega1/Table/Table.c
+++ Entrega1/Table/Table.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define TABLE_MAX_SIZE 100
 #define TABLE_LINE_MAX_SIZE 20
 #define TABLE_FILE_NAME "Table.txt"
 
 char tokenTable[TABLE_MAX_SIZE][TABLE_LINE_MAX_SIZE];
+int tableSize;
 
 int convertStringToInt(char *number) {
     int i = 0, lenght = strlen(number), convertedNumber = 0, pot = 1;
@@ -24,9 +26,9 @@ void addTokenOnTable(char *line) {
 }
 
 void buildTable() {
-  FILE *tableFile;
+  FILE *tableFile = fopen("TABLE_FILE_NAME", "r");
   char line[TABLE_LINE_MAX_SIZE];
-  tableFile = fopen("./table.txt", "r");
+  int count = 0;
 
   if (!tableFile) {
     printf("Erro ao ler o arquivo!");
@@ -34,5 +36,19 @@ void buildTable() {
 
   while(fgets(line, TABLE_LINE_MAX_SIZE, tableFile) != NULL) {
      addTokenOnTable(&line);
+     count++
   }
+
+  size = count;
+  fclose(tableFile);
+}
+
+bool hasOnTable(char *symbol) {
+  int i;
+  for (i = 0; i < size; i++) {
+    if (tokenTable[i] == symbol) {
+      return 1;
+    }
+  }
+  return 0;
 }
---
+++ Entrega1/Table/Table.h
@@ -0,0 +1,4 @@
+#include <stdbool.h>
+
+void buildTable();
+bool hasOnTable(char *symbol);
